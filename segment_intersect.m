5b6073a2e31e0d20a0a747568e227c309dcb1652
LGLucasG <lgaveriaux@enseirb-matmeca.fr>
2024-01-03 12:25:56 +0100

    just a comment

--- segment_intersect.m
+++ segment_intersect.m
@@ -1,4 +1,6 @@
 function [are_intersecting] = segment_intersect(seg_1, seg_2)
+% RETOURNE un booléen, vrai si les segment seg_1 et seg_2 s'intersectent,
+% faux sinon
 prod_1 = (seg_1(1,1) - seg_1(2,1)) * (seg_2(1,2) - seg_1(1,2)) - (seg_2(1,1) - seg_1(1,1)) * (seg_1(1,2) - seg_1(2,2));
 prod_2 = (seg_1(1,1) - seg_1(2,1)) * (seg_2(2,2) - seg_1(1,2)) - (seg_2(2,1) - seg_1(1,1)) * (seg_1(1,2) - seg_1(2,2));
 
